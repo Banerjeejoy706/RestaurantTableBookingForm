a11c98f0361ac271bd720e1e4c31dbdea5b4ed4c
Banerjeejoy706 <79395004+Banerjeejoy706@users.noreply.github.com>
2023-05-05 14:56:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("booking-form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const date = document.getElementById})
